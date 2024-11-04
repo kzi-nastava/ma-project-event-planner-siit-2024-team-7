bd74396c408a4aba2afcc4188b114a9c5ed6dbb3
David Makan <davemakan@gmail.com>
2024-11-04 20:39:25 +0100

    Set link from login to register screen

--- app/src/main/java/rs/ac/uns/eventplanner/team7/LoginScreen.java
+++ app/src/main/java/rs/ac/uns/eventplanner/team7/LoginScreen.java
@@ -1,6 +1,9 @@
 package rs.ac.uns.eventplanner.team7;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +17,14 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        TextView textView = findViewById(R.id.sign_up_link);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginScreen.this, RegistrationScreen.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override

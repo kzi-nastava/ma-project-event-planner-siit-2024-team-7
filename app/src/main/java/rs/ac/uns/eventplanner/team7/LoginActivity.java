61dc96f1c950690412611f3e66d7c681a988eb51
Patrik Barsi <patrik.barsi04@gmail.com>
2024-11-07 16:30:02 +0100

    Add login screen and routing to register

--- app/src/main/java/rs/ac/uns/eventplanner/team7/LoginActivity.java
+++ app/src/main/java/rs/ac/uns/eventplanner/team7/LoginActivity.java
@@ -3,8 +3,6 @@ package rs.ac.uns.eventplanner.team7;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import rs.ac.uns.eventplanner.team7.RegistrationScreen;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,23 +10,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import rs.ac.uns.eventplanner.team7.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textview.MaterialTextView;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button regButton = findViewById(R.id.reg_button);
-        regButton.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_login);
+
+        MaterialTextView signUp = findViewById(R.id.signupLink);
+        signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Create an Intent to start RegisterActivity
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                Intent intent = new Intent(LoginActivity.this, RegistrationScreen.class);
                 startActivity(intent);
             }
         });
-
     }
 }

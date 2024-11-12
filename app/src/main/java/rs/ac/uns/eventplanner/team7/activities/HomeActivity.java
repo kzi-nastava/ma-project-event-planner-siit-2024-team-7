35d90227ba509268a58813170f0bda2a0fc424d3
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2024-11-13 00:18:41 +0100

    Update HomeActivity and create HomePageAdapter Organized the 4 main fragments to tabs by using the TabLayout in combination with ViewPager2 with the help of HomePagerAdapter

--- app/src/main/java/rs/ac/uns/eventplanner/team7/activities/HomeActivity.java
+++ app/src/main/java/rs/ac/uns/eventplanner/team7/activities/HomeActivity.java
@@ -1,10 +1,16 @@
 package rs.ac.uns.eventplanner.team7.activities;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import rs.ac.uns.eventplanner.team7.R;
+import rs.ac.uns.eventplanner.team7.adapters.HomePagerAdapter;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -12,6 +18,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        initTabs();
     }
 
     @Override
@@ -42,6 +49,32 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    private void initTabs() {
+        TabLayout tabLayout = findViewById(R.id.home_page_tab_layout);
+        ViewPager2 viewPager = findViewById(R.id.home_page_view_pager);
+
+        HomePagerAdapter adapter = new HomePagerAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        // Link the TabLayout and ViewPager2
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            switch (position) {
+                case 0:
+                    tab.setText(R.string.tab_top_events);
+                    break;
+                case 1:
+                    tab.setText(R.string.tab_all_events);
+                    break;
+                case 2:
+                    tab.setText(R.string.tab_top_services_products);
+                    break;
+                case 3:
+                    tab.setText(R.string.tab_all_services_products);
+                    break;
+            }
+        }).attach();
     }
 
 }

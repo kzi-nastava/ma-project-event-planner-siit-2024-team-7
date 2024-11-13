bd7f30bfdf512256297b70694d86cfc2e3c78c19
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2024-11-13 22:35:20 +0100

    Remove excess comments

--- app/src/main/java/rs/ac/uns/eventplanner/team7/fragments/TopEventsFragment.java
+++ app/src/main/java/rs/ac/uns/eventplanner/team7/fragments/TopEventsFragment.java
@@ -21,7 +21,6 @@ public class TopEventsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_top_events, container, false);
     }
 }

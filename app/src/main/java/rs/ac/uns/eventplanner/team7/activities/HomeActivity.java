280b9c1dae00ef3a83992319d7ff58c0a8a5c3c2
Patrik Barsi <patrik.barsi04@gmail.com>
2024-11-13 14:26:28 +0100

    Add toolbar for search and profile options

--- app/src/main/java/rs/ac/uns/eventplanner/team7/activities/HomeActivity.java
+++ app/src/main/java/rs/ac/uns/eventplanner/team7/activities/HomeActivity.java
@@ -1,8 +1,15 @@
 package rs.ac.uns.eventplanner.team7.activities;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 
 import rs.ac.uns.eventplanner.team7.R;
 
@@ -12,6 +19,9 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 
     @Override
@@ -43,5 +53,24 @@ public class HomeActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_profile) {
+            View profileMenuItemView = findViewById(R.id.menu_profile);
+            PopupMenu popupMenu = new PopupMenu(this, profileMenuItemView);
+            popupMenu.getMenuInflater().inflate(R.menu.profile_menu, popupMenu.getMenu());
+            popupMenu.show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
 }

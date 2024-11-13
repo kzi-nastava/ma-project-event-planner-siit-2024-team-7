3633ad8779d26f0bfeb2c35c9f3fa3dbf85f6672
Patrik Barsi <patrik.barsi04@gmail.com>
2024-11-13 15:35:32 +0100

    Add functional logout MenuItem

--- app/src/main/java/rs/ac/uns/eventplanner/team7/activities/HomeActivity.java
+++ app/src/main/java/rs/ac/uns/eventplanner/team7/activities/HomeActivity.java
@@ -1,11 +1,13 @@
 package rs.ac.uns.eventplanner.team7.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.PopupMenu;
 import androidx.appcompat.widget.Toolbar;
@@ -65,8 +67,21 @@ public class HomeActivity extends AppCompatActivity {
             View profileMenuItemView = findViewById(R.id.menu_profile);
             PopupMenu popupMenu = new PopupMenu(this, profileMenuItemView);
             popupMenu.getMenuInflater().inflate(R.menu.profile_menu, popupMenu.getMenu());
+
+            popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(@NonNull MenuItem item) {
+                    if (item.getItemId() == R.id.logout) {
+                        // Handle logout action
+                        Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                        return true;
+                    }
+                    return false;
+                }
+            });
+
             popupMenu.show();
-            return true;
         }
         return super.onOptionsItemSelected(item);
     }

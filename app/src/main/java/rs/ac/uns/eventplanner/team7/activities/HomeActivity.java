65bd09af4dff4281e07f9dd37651fc64be9ff738
Patrik Barsi <patrik.barsi04@gmail.com>
2024-11-14 12:51:48 +0100

    Fix issues after merge

--- app/src/main/java/rs/ac/uns/eventplanner/team7/activities/HomeActivity.java
+++ app/src/main/java/rs/ac/uns/eventplanner/team7/activities/HomeActivity.java
@@ -70,15 +70,15 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.menu_profile) {
-            View profileMenuItemView = findViewById(R.id.menu_profile);
+        if (item.getItemId() == R.id.nav_account) {
+            View profileMenuItemView = findViewById(R.id.nav_account);
             PopupMenu popupMenu = new PopupMenu(this, profileMenuItemView);
             popupMenu.getMenuInflater().inflate(R.menu.profile_menu, popupMenu.getMenu());
 
             popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                 @Override
                 public boolean onMenuItemClick(@NonNull MenuItem item) {
-                    if (item.getItemId() == R.id.logout) {
+                    if (item.getItemId() == R.id.nav_logout) {
                         // Handle logout action
                         Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
                         startActivity(intent);
@@ -92,7 +92,6 @@ public class HomeActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
 
     private void initTabs() {
         TabLayout tabLayout = findViewById(R.id.home_page_tab_layout);

957c6846edd5ee0d27fc8d3f99ae3d00b7a25a84
Patrik Barsi <patrik.barsi04@gmail.com>
2024-12-18 19:41:45 +0100

    Connect to server, add one input field for phone

--- app/src/main/java/rs/ac/uns/eventplanner/team7/services/AuthService.java
+++ app/src/main/java/rs/ac/uns/eventplanner/team7/services/AuthService.java
@@ -7,6 +7,7 @@ import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import rs.ac.uns.eventplanner.team7.dto.auth.LoginRequestDTO;
 import rs.ac.uns.eventplanner.team7.dto.auth.LoginResponseDTO;
+import rs.ac.uns.eventplanner.team7.dto.auth.RegisterRequestDTO;
 
 public interface AuthService {
     @Headers({
@@ -15,5 +16,12 @@ public interface AuthService {
     })
     @POST("auth/login")
     Call<LoginResponseDTO> login(@Body LoginRequestDTO loginRequestDTO);
+
+    @Headers({
+            "User-Agent: Mobile-Android",
+            "Content-Type: application/json"
+    })
+    @POST("auth/register")
+    Call<Void> register(@Body RegisterRequestDTO registerRequestDTO);
 
 }

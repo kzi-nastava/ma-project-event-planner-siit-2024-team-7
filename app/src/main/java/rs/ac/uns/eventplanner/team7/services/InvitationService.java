5144a8b7b82e3b974250f6433cd14943cae4a46a
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2025-01-01 18:12:26 +0100

    Create InvitationService
    Used for making requests for sending and accepting invitations

---
+++ app/src/main/java/rs/ac/uns/eventplanner/team7/services/InvitationService.java
@@ -0,0 +1,24 @@
+package rs.ac.uns.eventplanner.team7.services;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+import rs.ac.uns.eventplanner.team7.dto.invitation.InvitationAcceptanceDTO;
+
+public interface InvitationService {
+
+    @Headers({
+            "User-Agent: Mobile-Android",
+            "Content-Type: application/json"
+    })
+    @POST("invitations/send")
+    Call<String> sendInvitations(@Body InvitationAcceptanceDTO dto);
+
+    @Headers({
+            "User-Agent: Mobile-Android",
+            "Content-Type: application/json"
+    })
+    @POST("invitations/accept_from_app")
+    Call<String> acceptInvitation(@Body InvitationAcceptanceDTO dto);
+}

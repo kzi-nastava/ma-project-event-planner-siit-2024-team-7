c61a7c3191ae9169f24db9f1a77b24717e8a7cf0
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2025-01-02 18:07:52 +0100

    Update TopEvents and TopItems fragments
    They are now connected to backend

--- app/src/main/java/rs/ac/uns/eventplanner/team7/fragments/TopEventsFragment.java
+++ app/src/main/java/rs/ac/uns/eventplanner/team7/fragments/TopEventsFragment.java
@@ -1,18 +1,31 @@
 package rs.ac.uns.eventplanner.team7.fragments;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.textview.MaterialTextView;
+
+import java.util.List;
+import java.util.Objects;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import rs.ac.uns.eventplanner.team7.R;
+import rs.ac.uns.eventplanner.team7.adapters.HomeRecyclerViewAdapter;
+import rs.ac.uns.eventplanner.team7.dto.event.DetailedEventDTO;
+import rs.ac.uns.eventplanner.team7.services.EventService;
+import rs.ac.uns.eventplanner.team7.utils.ClientUtils;
+import rs.ac.uns.eventplanner.team7.utils.JwtUtil;
 
 public class TopEventsFragment extends Fragment {
+    private final EventService service = ClientUtils.injectService(EventService.class);
 
     public TopEventsFragment() {
         // Required empty public constructor
@@ -21,6 +34,40 @@ public class TopEventsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_top_events, container, false);
+        View view = inflater.inflate(R.layout.fragment_top_events, container, false);
+        setupView(view);
+        return view;
+    }
+
+    private void setupView(View view) {
+        String userCity = JwtUtil.getCity(requireContext());
+        RecyclerView eventsView = view.findViewById(R.id.top_events_recycler_view);
+        MaterialTextView messageView = view.findViewById(R.id.top_events_message_view);
+        service.findTopFive(userCity).enqueue(new Callback<>() {
+            @Override
+            public void onResponse(@NonNull Call<List<DetailedEventDTO>> call,
+                                   @NonNull Response<List<DetailedEventDTO>> response) {
+
+                if (!response.isSuccessful()) {
+                    messageView.setText(R.string.unable_to_contact_server);
+                    return;
+                }
+                var events = response.body();
+                if (Objects.requireNonNull(events).isEmpty()) {
+                    messageView.setText(R.string.no_events_to_show);
+                    return;
+                }
+                messageView.setVisibility(View.GONE);
+                eventsView.setAdapter(new HomeRecyclerViewAdapter<>(requireContext(),
+                        events, true));
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<List<DetailedEventDTO>> call, @NonNull Throwable t) {
+                messageView.setText(R.string.unable_to_contact_server);
+                messageView.setVisibility(View.VISIBLE);
+                eventsView.setVisibility(View.GONE);
+            }
+        });
     }
 }
--- app/src/main/java/rs/ac/uns/eventplanner/team7/fragments/TopItemsFragment.java
+++ app/src/main/java/rs/ac/uns/eventplanner/team7/fragments/TopItemsFragment.java
@@ -1,16 +1,38 @@
 package rs.ac.uns.eventplanner.team7.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.textview.MaterialTextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import rs.ac.uns.eventplanner.team7.R;
+import rs.ac.uns.eventplanner.team7.adapters.HomeRecyclerViewAdapter;
+import rs.ac.uns.eventplanner.team7.dto.item.DetailedItemDTO;
+import rs.ac.uns.eventplanner.team7.services.ProductService;
+import rs.ac.uns.eventplanner.team7.services.ServiceService;
+import rs.ac.uns.eventplanner.team7.utils.ClientUtils;
+import rs.ac.uns.eventplanner.team7.utils.JwtUtil;
 
 public class TopItemsFragment extends Fragment {
+    private MaterialTextView messageView;
+    private RecyclerView topItemsView;
+    private final ProductService productService = ClientUtils.injectService(ProductService.class);
+    private final ServiceService serviceService = ClientUtils.injectService(ServiceService.class);
+    private final List<DetailedItemDTO> serviceResponses = new ArrayList<>();
+    private final AtomicInteger responseCount = new AtomicInteger(0);
 
     public TopItemsFragment() {
         // Required empty public constructor
@@ -19,6 +41,58 @@ public class TopItemsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_top_items, container, false);
+        View view = inflater.inflate(R.layout.fragment_top_items, container, false);
+        setupView(view);
+        return view;
+    }
+
+    private void setupView(View view) {
+
+        String userCity = JwtUtil.getCity(requireContext());
+        topItemsView = view.findViewById(R.id.top_items_recycler_view);
+        messageView = view.findViewById(R.id.top_items_message_view);
+
+        handleServiceResponse(productService.findTopFive(userCity));
+        handleServiceResponse(serviceService.findTopFive(userCity));
+    }
+
+    private void handleServiceResponse(Call<List<DetailedItemDTO>> service) {
+        service.enqueue(new Callback<>() {
+            @Override
+            public void onResponse(@NonNull Call<List<DetailedItemDTO>> call,
+                                   @NonNull Response<List<DetailedItemDTO>> response) {
+
+                if (!response.isSuccessful()) {
+                    tryToSetAdapter();
+                    return;
+                }
+
+                List<DetailedItemDTO> items = response.body();
+                if (items != null && !items.isEmpty()) {
+                    synchronized (serviceResponses) {
+                        messageView.setVisibility(View.GONE);
+                        serviceResponses.addAll(items);
+                    }
+                }
+                tryToSetAdapter();
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<List<DetailedItemDTO>> call, @NonNull Throwable t) {
+                tryToSetAdapter();
+            }
+        });
+    }
+
+    private void tryToSetAdapter() {
+        if (responseCount.incrementAndGet() == 2) {
+            if (serviceResponses.isEmpty()) {
+                messageView.setText(R.string.no_items_to_show);
+                return;
+            }
+            messageView.setVisibility(View.GONE);
+            topItemsView.setAdapter(new HomeRecyclerViewAdapter<>(requireContext(),
+                    serviceResponses, true));
+        }
     }
 }

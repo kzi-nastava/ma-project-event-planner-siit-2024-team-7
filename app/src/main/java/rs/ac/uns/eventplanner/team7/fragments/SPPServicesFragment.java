a995519c63d33f19d33ede010c497cda7f58bcb9
David Makan <davemakan@gmail.com>
2025-01-06 20:00:31 +0100

    Implemented method for searching services Adjusted filters and connected it with backend

--- app/src/main/java/rs/ac/uns/eventplanner/team7/fragments/SPPServicesFragment.java
+++ app/src/main/java/rs/ac/uns/eventplanner/team7/fragments/SPPServicesFragment.java
@@ -5,24 +5,37 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textview.MaterialTextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import rs.ac.uns.eventplanner.team7.R;
 import rs.ac.uns.eventplanner.team7.adapters.CardRecyclerViewAdapter;
 import rs.ac.uns.eventplanner.team7.dto.Page;
+import rs.ac.uns.eventplanner.team7.dto.Sort;
 import rs.ac.uns.eventplanner.team7.dto.item.BasicItemDTO;
 import rs.ac.uns.eventplanner.team7.model.interfaces.CardClickListener;
 import rs.ac.uns.eventplanner.team7.model.interfaces.SearchActionsListener;
 import rs.ac.uns.eventplanner.team7.services.ServiceService;
 import rs.ac.uns.eventplanner.team7.utils.ClientUtils;
+import rs.ac.uns.eventplanner.team7.utils.JwtUtil;
 
 public class SPPServicesFragment extends Fragment implements SearchActionsListener, CardClickListener {
     private final ServiceService serviceService = ClientUtils.injectService(ServiceService.class);
@@ -30,8 +43,12 @@ public class SPPServicesFragment extends Fragment implements SearchActionsListen
     private final ServiceFilterFragment filterFragment;
     private final Page<BasicItemDTO> page;
     private Map<String, String> latestFilters;
+    private String lastSearch;
+    private SearchView searchView;
+    private MaterialTextView messageView;
     private RecyclerView servicesView;
     private CardRecyclerViewAdapter<BasicItemDTO> viewAdapter;
+    private boolean isLoading;
 
     public SPPServicesFragment() {
         page = Page.getDefault();
@@ -43,8 +60,10 @@ public class SPPServicesFragment extends Fragment implements SearchActionsListen
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_spp_services, container, false);
+        messageView = view.findViewById(R.id.services_search_result_message_view);
+        searchView = view.findViewById(R.id.spp_services_search_view);
         servicesView = view.findViewById(R.id.spp_services_recycler_view);
-        viewAdapter = new CardRecyclerViewAdapter<>(requireContext(), new ArrayList<>(), true, this);
+        viewAdapter = new CardRecyclerViewAdapter<>(requireContext(), new ArrayList<>(), false, this);
         servicesView.setAdapter(viewAdapter);
         return view;
     }
@@ -53,31 +72,132 @@ public class SPPServicesFragment extends Fragment implements SearchActionsListen
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        setContent(false);
+        setupSearchView();
 
+        MaterialButton filtersButton = view.findViewById(R.id.service_filters_button);
+        filtersButton.setOnClickListener(v -> filterFragment.show(getChildFragmentManager(),
+                filterFragment.getTag()));
+
+        setupContentScrollListener();
     }
 
     @Override
     public void onSortApplied() {
-
+        page.resetToDefault();
+        page.setSort(new Sort().by(""));
+        setContent(false);
     }
 
     @Override
     public void onNextPage() {
-
+        if (isLoading || page.isLast()) return;
+        page.nextPage();
+        setContent(true);
     }
 
     @Override
     public void onFiltersApplied() {
-
+        page.resetToDefault();
+        String query = latestFilters.get("name");
+        latestFilters = filterFragment.getFilters();
+        latestFilters.put("name", query);
+        setContent(false);
     }
 
     @Override
     public void onFiltersReset() {
-
+        page.resetToDefault();
+        latestFilters = new HashMap<>();
+        searchView.setQuery("", false);
+        searchView.clearFocus();
+        setContent(false);
     }
 
     @Override
     public void onCardClicked(Integer entityId, String type) {
 
+    }
+
+    private void setContent(boolean isUpdate) {
+        isLoading = true;
+        Map<String, String> combinedFilters = combineFiltersAndSort();
+        serviceService.findServicesByProvider(JwtUtil.getAuthorizationValue(requireContext()), combinedFilters).enqueue(new Callback<Page<BasicItemDTO>>() {
+            @Override
+            public void onResponse(@NonNull Call<Page<BasicItemDTO>> call,
+                                   @NonNull Response<Page<BasicItemDTO>> response) {
+                if (!isUpdate) viewAdapter.clear();
+                if (!response.isSuccessful()) {
+                    messageView.setVisibility(View.VISIBLE);
+                    messageView.setText(R.string.unable_to_contact_server);
+                    return;
+                }
+                Page<BasicItemDTO> pagedResponse = response.body();
+                if (pagedResponse == null || pagedResponse.isEmpty()) {
+                    if (page.isFirst()) {
+                        messageView.setText(R.string.no_services_to_show);
+                        messageView.setVisibility(View.VISIBLE);
+                    }
+                    return;
+                }
+                page.update(pagedResponse);
+                String resultCount = String.format(getString(R.string.d_services_found),
+                        page.getTotalElements());
+                messageView.setText(resultCount);
+                viewAdapter.addAll(page.getContent());
+                isLoading = false;
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Page<BasicItemDTO>> call, @NonNull Throwable t) {
+                messageView.setVisibility(View.VISIBLE);
+                messageView.setText(R.string.error_fetching_more_data);
+                isLoading = false;
+            }
+        });
+    }
+
+    private Map<String, String> combineFiltersAndSort() {
+        var combined = Stream.concat(latestFilters.entrySet().stream(), page.toQueryMap().entrySet().stream());
+        var combinedAndSorted = combined.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        combinedAndSorted.put("sort", "");
+        return combinedAndSorted;
+    }
+
+    private void setupContentScrollListener() {
+        servicesView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                var layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+                int lastVisiblePosition = Objects.requireNonNull(layoutManager).findLastCompletelyVisibleItemPosition();
+                if (lastVisiblePosition == viewAdapter.getLastItemIndex() && !page.isLast())
+                    onNextPage();
+            }
+        });
+    }
+
+    private void setupSearchView() {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (query.isEmpty()) {
+                    return true;
+                }
+                latestFilters.put("name", query);
+                searchView.clearFocus();
+                setContent(false);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (newText.equals(lastSearch)) {
+                    onQueryTextSubmit(lastSearch);
+                }
+                lastSearch = newText;
+                return true;
+            }
+        });
     }
 }

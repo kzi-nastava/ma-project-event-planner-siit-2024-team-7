8a93ca10c551cdb69c4fa3b7f67f804869422f2c
David Makan <davemakan@gmail.com>
2025-01-27 10:57:49 +0100

    Adjust DTOs

--- app/src/main/java/rs/ac/uns/eventplanner/team7/dto/product/GetProductResponseDTO.java
+++ app/src/main/java/rs/ac/uns/eventplanner/team7/dto/product/GetProductResponseDTO.java
@@ -1,36 +1,22 @@
-package rs.ac.uns.eventplanner.team7.dto.service;
+package rs.ac.uns.eventplanner.team7.dto.product;
 
 import java.util.Set;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import rs.ac.uns.eventplanner.team7.dto.pricing.PricingResponseDTO;
+import rs.ac.uns.eventplanner.team7.dto.service.WorkDayDTO;
 import rs.ac.uns.eventplanner.team7.model.Category;
 import rs.ac.uns.eventplanner.team7.model.EventType;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class GetServiceResponseDTO {
+public class GetProductResponseDTO {
     private Integer id;
     private String name;
     private String description;
     private Set<String> images;
     private PricingResponseDTO pricing;
     private Category category;
-    private String specifics;
-    private Set<WorkDayDTO> workDaysDTOs;
-    private int minDurationInMinutes;
-    private int maxDurationInMinutes;
-    private int reservationDeadlineInDays;
-    private int cancellationDeadlineInDays;
     private Set<EventType> appliesTo;
     private boolean own;
     private boolean visible;
     private boolean available;
     private boolean favourite;
-    private boolean reserved;
 }

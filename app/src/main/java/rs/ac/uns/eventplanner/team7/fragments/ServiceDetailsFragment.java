a79c1b7fae56a291f9b2da73e2fec94b307f2c8e
David Makan <davemakan@gmail.com>
2025-01-27 18:31:49 +0100

    Display information on service details page

--- app/src/main/java/rs/ac/uns/eventplanner/team7/fragments/ServiceDetailsFragment.java
+++ app/src/main/java/rs/ac/uns/eventplanner/team7/fragments/ServiceDetailsFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,8 +14,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textview.MaterialTextView;
 
 import java.util.Objects;
 
@@ -33,6 +38,8 @@ public class ServiceDetailsFragment extends Fragment {
     private final GetServiceResponseDTO serviceDTO;
 
     ImageView favouriteStar;
+    MaterialTextView nameView, descriptionView, specificsView, priceView, discountView, minDurationView, maxDurationView,
+            reservationDeadlineView, cancellationDeadlineView, categoryView;
 
     public ServiceDetailsFragment(GetServiceResponseDTO serviceDTO) {
         this.serviceDTO = serviceDTO;
@@ -46,6 +53,18 @@ public class ServiceDetailsFragment extends Fragment {
         if (serviceDTO.isFavourite())
             favouriteStar.setImageResource(R.drawable.ic_star_filled);
 
+        nameView = view.findViewById(R.id.service_details_name);
+        descriptionView = view.findViewById(R.id.service_details_description);
+        specificsView = view.findViewById(R.id.service_details_specifics);
+        priceView = view.findViewById(R.id.service_details_price);
+        discountView = view.findViewById(R.id.service_details_discount);
+        minDurationView = view.findViewById(R.id.service_details_min_duration);
+        maxDurationView = view.findViewById(R.id.service_details_max_duration);
+        reservationDeadlineView = view.findViewById(R.id.service_details_reservation);
+        cancellationDeadlineView = view.findViewById(R.id.service_details_cancellation);
+        categoryView = view.findViewById(R.id.service_details_category);
+
+        fillDetails();
         return view;
     }
 
@@ -80,5 +99,18 @@ public class ServiceDetailsFragment extends Fragment {
                         }
                     });
         });
+    }
+
+    private void fillDetails() {
+        nameView.setText(serviceDTO.getName());
+        descriptionView.setText(serviceDTO.getDescription());
+        specificsView.setText(serviceDTO.getSpecifics());
+        priceView.setText(String.format("%s €", serviceDTO.getPricing().getPrice()));
+        discountView.setText(String.format("%s %%", serviceDTO.getPricing().getDiscount()));
+        minDurationView.setText(String.format("%s min", serviceDTO.getMinDurationInMinutes()));
+        maxDurationView.setText(String.format("%s min", serviceDTO.getMaxDurationInMinutes()));
+        reservationDeadlineView.setText(String.format("%s d", serviceDTO.getReservationDeadlineInDays()));
+        cancellationDeadlineView.setText(String.format("%s d", serviceDTO.getCancellationDeadlineInDays()));
+        categoryView.setText(serviceDTO.getCategory().getName());
     }
 }

7f6723b3027556b37ddee450107782032aad23b4
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2025-02-01 13:15:33 +0100

    Update GetProductResponseDTO and GetServiceResponseDTO
    Made GetServiceResponseDTO extend GetProductResponseDTO
    
    GetProductResponseDTO implemented parcelable interface

--- app/src/main/java/rs/ac/uns/eventplanner/team7/dto/product/GetProductResponseDTO.java
+++ app/src/main/java/rs/ac/uns/eventplanner/team7/dto/product/GetProductResponseDTO.java
@@ -1,6 +1,11 @@
 package rs.ac.uns.eventplanner.team7.dto.product;
 
-import java.util.Set;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,16 +19,71 @@ import rs.ac.uns.eventplanner.team7.model.EventType;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class GetProductResponseDTO {
-    private Integer id;
-    private String name;
-    private String description;
-    private Set<String> images;
-    private PricingResponseDTO pricing;
-    private Category category;
-    private Set<EventType> appliesTo;
-    private boolean own;
-    private boolean visible;
-    private boolean available;
-    private boolean favourite;
+public class GetProductResponseDTO implements Parcelable {
+    protected Integer id;
+    protected String name;
+    protected String description;
+    protected List<String> images;
+    protected PricingResponseDTO pricing;
+    protected Category category;
+    protected List<EventType> appliesTo;
+    protected boolean own;
+    protected boolean visible;
+    protected boolean available;
+    protected boolean favourite;
+
+    protected GetProductResponseDTO(Parcel in) {
+        if (in.readByte() == 0) {
+            id = null;
+        } else {
+            id = in.readInt();
+        }
+        name = in.readString();
+        description = in.readString();
+        images = in.createStringArrayList();
+        pricing = in.readParcelable(PricingResponseDTO.class.getClassLoader(), PricingResponseDTO.class);
+        category = in.readParcelable(Category.class.getClassLoader(), Category.class);
+        appliesTo = in.createTypedArrayList(EventType.CREATOR);
+        own = in.readByte() != 0;
+        visible = in.readByte() != 0;
+        available = in.readByte() != 0;
+        favourite = in.readByte() != 0;
+    }
+
+    public static final Creator<GetProductResponseDTO> CREATOR = new Creator<>() {
+        @Override
+        public GetProductResponseDTO createFromParcel(Parcel in) {
+            return new GetProductResponseDTO(in);
+        }
+
+        @Override
+        public GetProductResponseDTO[] newArray(int size) {
+            return new GetProductResponseDTO[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        if (id == null) {
+            dest.writeByte((byte) 0);
+        } else {
+            dest.writeByte((byte) 1);
+            dest.writeInt(id);
+        }
+        dest.writeString(name);
+        dest.writeString(description);
+        dest.writeStringList(images);
+        dest.writeParcelable(pricing, flags);
+        dest.writeParcelable(category, flags);
+        dest.writeTypedList(appliesTo);
+        dest.writeByte((byte) (own ? 1 : 0));
+        dest.writeByte((byte) (visible ? 1 : 0));
+        dest.writeByte((byte) (available ? 1 : 0));
+        dest.writeByte((byte) (favourite ? 1 : 0));
+    }
 }
--- app/src/main/java/rs/ac/uns/eventplanner/team7/dto/service/GetServiceResponseDTO.java
+++ app/src/main/java/rs/ac/uns/eventplanner/team7/dto/service/GetServiceResponseDTO.java
@@ -7,61 +7,35 @@ import androidx.annotation.NonNull;
 
 import java.util.List;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import rs.ac.uns.eventplanner.team7.dto.pricing.PricingResponseDTO;
-import rs.ac.uns.eventplanner.team7.model.Category;
-import rs.ac.uns.eventplanner.team7.model.EventType;
+import rs.ac.uns.eventplanner.team7.dto.product.GetProductResponseDTO;
 
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
-public class GetServiceResponseDTO implements Parcelable {
-    private Integer id;
-    private String name;
-    private String description;
-    private List<String> images;
-    private PricingResponseDTO pricing;
-    private Category category;
+public class GetServiceResponseDTO extends GetProductResponseDTO implements Parcelable {
     private String specifics;
     private List<WorkDayDTO> workDaysDTOs;
     private int minDurationInMinutes;
     private int maxDurationInMinutes;
     private int reservationDeadlineInDays;
     private int cancellationDeadlineInDays;
-    private List<EventType> appliesTo;
-    private boolean own;
-    private boolean visible, available, favourite, reserved;
+    private boolean reserved;
 
     protected GetServiceResponseDTO(Parcel in) {
-        if (in.readByte() == 0) {
-            id = null;
-        } else {
-            id = in.readInt();
-        }
-        name = in.readString();
-        description = in.readString();
-        images = in.createStringArrayList();
-        pricing = in.readParcelable(PricingResponseDTO.class.getClassLoader());
-        category = in.readParcelable(Category.class.getClassLoader());
+        super(in);
         specifics = in.readString();
         workDaysDTOs = in.createTypedArrayList(WorkDayDTO.CREATOR);
         minDurationInMinutes = in.readInt();
         maxDurationInMinutes = in.readInt();
         reservationDeadlineInDays = in.readInt();
         cancellationDeadlineInDays = in.readInt();
-        appliesTo = in.createTypedArrayList(EventType.CREATOR);
-        own = in.readByte() != 0;
-        visible = in.readByte() != 0;
-        available = in.readByte() != 0;
-        favourite = in.readByte() != 0;
         reserved = in.readByte() != 0;
     }
 
-    public static final Creator<GetServiceResponseDTO> CREATOR = new Creator<GetServiceResponseDTO>() {
+    public static final Creator<GetServiceResponseDTO> CREATOR = new Creator<>() {
         @Override
         public GetServiceResponseDTO createFromParcel(Parcel in) {
             return new GetServiceResponseDTO(in);
@@ -80,28 +54,13 @@ public class GetServiceResponseDTO implements Parcelable {
 
     @Override
     public void writeToParcel(@NonNull Parcel dest, int flags) {
-        if (id == null) {
-            dest.writeByte((byte) 0);
-        } else {
-            dest.writeByte((byte) 1);
-            dest.writeInt(id);
-        }
-        dest.writeString(name);
-        dest.writeString(description);
-        dest.writeStringList(images);
-        dest.writeParcelable(pricing, flags);
-        dest.writeParcelable(category, flags);
+        super.writeToParcel(dest, flags);
         dest.writeString(specifics);
         dest.writeTypedList(workDaysDTOs);
         dest.writeInt(minDurationInMinutes);
         dest.writeInt(maxDurationInMinutes);
         dest.writeInt(reservationDeadlineInDays);
         dest.writeInt(cancellationDeadlineInDays);
-        dest.writeTypedList(appliesTo);
-        dest.writeByte((byte) (own ? 1 : 0));
-        dest.writeByte((byte) (visible ? 1 : 0));
-        dest.writeByte((byte) (available ? 1 : 0));
-        dest.writeByte((byte) (favourite ? 1 : 0));
         dest.writeByte((byte) (reserved ? 1 : 0));
     }
 }

43c6b8e849f6b8a706519f2cefedb58d7456891a
David Makan <davemakan@gmail.com>
2025-02-22 19:35:26 +0100

    Fix WebSocketService and add notifications Added request for permission for sending notifications Added NotificationUtils Added necessary dependencies

--- app/src/main/java/rs/ac/uns/eventplanner/team7/activities/HomeActivity.java
+++ app/src/main/java/rs/ac/uns/eventplanner/team7/activities/HomeActivity.java
@@ -1,14 +1,19 @@
 package rs.ac.uns.eventplanner.team7.activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
@@ -32,6 +37,7 @@ import rs.ac.uns.eventplanner.team7.model.enums.UserRole;
 import rs.ac.uns.eventplanner.team7.services.InvitationService;
 import rs.ac.uns.eventplanner.team7.utils.ClientUtils;
 import rs.ac.uns.eventplanner.team7.utils.JwtUtil;
+import rs.ac.uns.eventplanner.team7.utils.NotificationUtils;
 import rs.ac.uns.eventplanner.team7.utils.WebSocketService;
 
 public class HomeActivity extends AppCompatActivity {
@@ -61,8 +67,16 @@ public class HomeActivity extends AppCompatActivity {
 
         if (getIntent().getExtras() != null) handleInvitationAccepting();
 
-        webSocketService = new WebSocketService();
+        webSocketService = new WebSocketService(this);
         webSocketService.connect(JwtUtil.getToken(this));
+
+        NotificationUtils.createNotificationChannel(this);
+        ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(
+                new ActivityResultContracts.RequestPermission(),
+                isGranted -> {});
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
+                != PackageManager.PERMISSION_GRANTED)
+            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS);
     }
 
     @Override

b821fb63494e6793b74b4a6f341bf678e5b72eb8
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2025-08-27 18:10:50 +0200

    Create ContactInfoDialogFragment
    It shows profile picture and the name of the chat recipient which will replace that usage in ChatsFragment, and also has buttons to view profile (won't be implemented on this branch) and to block the user

---
+++ app/src/main/java/rs/ac/uns/eventplanner/team7/ui/fragments/chats/ContactInfoDialogFragment.java
@@ -0,0 +1,101 @@
+package rs.ac.uns.eventplanner.team7.ui.fragments.chats;
+
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textview.MaterialTextView;
+import com.squareup.picasso.Picasso;
+
+import lombok.Setter;
+import rs.ac.uns.eventplanner.team7.BuildConfig;
+import rs.ac.uns.eventplanner.team7.R;
+
+
+public class ContactInfoDialogFragment extends BottomSheetDialogFragment {
+
+    public interface OnActionClickListener {
+        void OnBlockUserClicked();
+        void OnViewProfileClicked();
+    }
+
+    private static final String ARG_CONTACT_NAME = "contactName";
+    private static final String ARG_CONTACT_IMAGE_URL = "contactImageUrl";
+
+    private String contactName, contactImageUrl;
+
+    @Setter
+    private OnActionClickListener onActionClickListener;
+
+    private ImageView profilePicView;
+
+    public ContactInfoDialogFragment() {
+        // Required empty public constructor
+    }
+
+    public static ContactInfoDialogFragment newInstance(String contactName, String contactImageUrl) {
+        ContactInfoDialogFragment fragment = new ContactInfoDialogFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_CONTACT_NAME, contactName);
+        args.putString(ARG_CONTACT_IMAGE_URL, contactImageUrl);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            contactName = getArguments().getString(ARG_CONTACT_NAME);
+            contactImageUrl = getArguments().getString(ARG_CONTACT_IMAGE_URL);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_contact_info_dialog, container, false);
+        profilePicView = view.findViewById(R.id.chat_contract_profile_pic);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        String backendUrl = "http://" + BuildConfig.IP_ADDR + ":8080/api/images?imageUrl=" + contactImageUrl;
+        if (contactImageUrl != null && !contactImageUrl.isEmpty()) {
+            Picasso.get()
+                    .load(backendUrl)
+                    .placeholder(R.drawable.image_placeholder)
+                    .error(R.drawable.image_placeholder)
+                    .into(profilePicView);
+        }
+        MaterialTextView contactNameView = view.findViewById(R.id.chat_contact_name);
+        contactNameView.setText(contactName);
+
+        MaterialButton closeDialogButton = view.findViewById(R.id.close_dialog_button);
+        closeDialogButton.setOnClickListener(v -> dismiss());
+
+        MaterialButton blockAccountButton = view.findViewById(R.id.block_account_button);
+        blockAccountButton.setOnClickListener(v -> {
+            onActionClickListener.OnBlockUserClicked();
+            dismiss();
+        });
+
+        MaterialButton viewProfileButton = view.findViewById(R.id.view_profile_button);
+        viewProfileButton.setOnClickListener(v -> {
+            onActionClickListener.OnViewProfileClicked();
+            dismiss();
+        });
+
+    }
+}
